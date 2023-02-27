3e565932eb92dbeefb26a7c24742df89e73fc4ce
Yathusan A <100049350+Ya2s4n@users.noreply.github.com>
2023-02-27 19:52:05 +0000

    views and text

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>What's up my neighbour</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.thinText}>
+          <Text> What's up my neighbour</Text>
+          <Text> What's up my neighbour</Text>
+
+        </Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +26,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  header: {
+    backgroundColor: 'cyan',
+    padding: 50,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  thinText:{
+
+    fontWeight: 'thin'
+  },
+  body:{
+    backgroundColor: 'green',
+    padding: 50,
+  }
+
+}); 
